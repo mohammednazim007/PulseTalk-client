52b96843827bb745dbc71fc1706aecd9524df9c9
mohammednazim007 <mohammednazim3629@gmail.com>
2025-10-01 20:29:55 +0600

    feat: init socket io

---
+++ src/app/socket-io/socket-io.ts
@@ -0,0 +1,23 @@
+import { io } from "socket.io-client";
+
+let socket: any = null;
+
+export const connectSocket = (user_id: string) => {
+  socket = io(process.env.NEXT_PUBLIC_SOCKET_URL as string, {
+    query: { user_id },
+    transports: ["websocket"],
+  });
+
+  return socket;
+};
+
+// ** Get Socket
+export const getSocket = () => socket;
+
+// ** Disconnect Socket
+export const disconnectSocket = () => {
+  if (socket) {
+    socket.disconnect();
+    socket = null;
+  }
+};
